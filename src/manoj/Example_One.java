c8cbac8b6e34740a089abc0efcb482effedbba1a
user <user@LAPTOP-GEC1AM3G>
2020-12-08 22:09:23 -0500

    added another statement

--- src/manoj/Example_One.java
+++ src/manoj/Example_One.java
@@ -5,6 +5,7 @@ public class Example_One {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("\n Hello World!!! \n");
+		System.out.println("\n narendra!!! \n");
 	}
 
 }
