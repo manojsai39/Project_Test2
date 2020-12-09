757a6bf3016b06e587dcab1dd6037f0103923c42
arvind <arvind@LAPTOP-QMT0MAAJ>
2020-12-08 22:08:49 -0500

    added another display

--- src/manoj/Example_One.java
+++ src/manoj/Example_One.java
@@ -4,7 +4,9 @@ public class Example_One {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
 		System.out.println("\n Hello World!!! \n");
+		System.out.println("\n arvind!!! \n");
 	}
 
 }
