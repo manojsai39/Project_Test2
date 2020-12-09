2757941b667161e51a401fc5075e86f02170d720
Manoj <Manoj@192.168.2.26>
2020-12-08 22:05:30 -0500

    Added a Class

---
+++ src/manoj/Example_One.java
@@ -0,0 +1,10 @@
+package manoj;
+
+public class Example_One {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("\n Hello World!!! \n");
+	}
+
+}
